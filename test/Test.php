763ac963c5f7ad55a65174974e54992df0348965
manayop <dedanone1>
2016-04-02 16:07:34 +0200

    refactorizacion test

--- test/Test.php
+++ test/Test.php
@@ -5,6 +5,35 @@ include_once 'Configuration.php';
 class Test extends PHPUnit_Framework_TestCase
 {
 
+    private $defaults = array(
+        'code' => '',
+        'min_length' => 5,
+        'max_length' => 5,
+        'backgrounds' => array(
+            '/app/backgrounds/45-degree-fabric.png',
+            '/app/backgrounds/cloth-alike.png',
+            '/app/backgrounds/grey-sandbag.png',
+            '/app/backgrounds/kinda-jean.png',
+            '/app/backgrounds/polyester-lite.png',
+            '/app/backgrounds/stitched-wool.png',
+            '/app/backgrounds/white-carbon.png',
+            '/app/backgrounds/white-wave.png'
+        ),
+        'fonts' => array(
+            '/app/fonts/times_new_yorker.ttf'
+        ),
+        'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
+        'min_font_size' => 28,
+        'max_font_size' => 28,
+        'color' => '#666',
+        'angle_min' => 0,
+        'angle_max' => 10,
+        'shadow' => true,
+        'shadow_color' => '#fff',
+        'shadow_offset_x' => -1,
+        'shadow_offset_y' => 1
+    );
+
     public function testOpts()
     {
         $this->assertInstanceOf('Configuration',new Configuration());
@@ -13,54 +42,30 @@ class Test extends PHPUnit_Framework_TestCase
     public function testDefaults()
     {
         $configuration = new Configuration();
-
-        $bg_path = '/app/backgrounds/';
-
-        $defaults = array(
-            'code' => '',
-            'min_length' => 5,
-            'max_length' => 5,
-            'backgrounds' => array(
-                $bg_path . '45-degree-fabric.png',
-                $bg_path . 'cloth-alike.png',
-                $bg_path . 'grey-sandbag.png',
-                $bg_path . 'kinda-jean.png',
-                $bg_path . 'polyester-lite.png',
-                $bg_path . 'stitched-wool.png',
-                $bg_path . 'white-carbon.png',
-                $bg_path . 'white-wave.png'
-            ),
-            'fonts' => array(
-                '/app/fonts/times_new_yorker.ttf'
-            ),
-            'characters' => 'ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnprstuvwxyz23456789',
-            'min_font_size' => 28,
-            'max_font_size' => 28,
-            'color' => '#666',
-            'angle_min' => 0,
-            'angle_max' => 10,
-            'shadow' => true,
-            'shadow_color' => '#fff',
-            'shadow_offset_x' => -1,
-            'shadow_offset_y' => 1
-        );
-
         $asHash = $configuration->asHash();
-        $this->assertEquals($defaults,$asHash);
+        $this->assertEquals($this->defaults, $asHash);
 
+    }
+
+    public function testNullConfiguration()
+    {
         $nullConfiguration = new Configuration(null);
-        $this->assertEquals($defaults,$nullConfiguration->asHash());
+        $this->assertEquals($this->defaults, $nullConfiguration->asHash());
+    }
 
+    public function testDefaultsNotOverwrite()
+    {
         $notNullConfiguration = new Configuration(array(
             min_font_size => 10,
             max_font_size => 11
         ));
 
-        $this->assertEquals($notNullConfiguration->asHash()['min_font_size'],10);
-        $this->assertEquals($notNullConfiguration->asHash()['max_font_size'],11);
+        $this->assertEquals($notNullConfiguration->asHash()['min_font_size'], 10);
+        $this->assertEquals($notNullConfiguration->asHash()['max_font_size'], 11);
 
 
 
     }
+
 
 }
