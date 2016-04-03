0b4755062616dbb80994a544fd1ca621628faceb
manayop <dedanone1>
2016-04-03 19:52:45 +0200

    generacion de clase nueva y test correspondiente

---
+++ CaptchaGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: manayop
+ * Date: 3/04/16
+ * Time: 19:51
+ */
+class CaptchaGenerator
+{
+
+}
---
+++ test/CaptchaGeneratorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "CaptchaGenerator.php";
+
+
+class CaptchaGeneratorTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGenerator()
+    {
+        $this->assertInstanceOf('CaptchaGenerator',new CaptchaGenerator());
+    }
+
+
+}
