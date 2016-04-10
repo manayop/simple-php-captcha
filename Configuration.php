e71c174c351b0735beb461d06f018537089fc50d
manayop <dedanone1>
2016-04-10 22:38:02 +0200

    reorganizacion de excepciones

--- Configuration.php
+++ Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'FileNotExistsException.php';
+include_once 'exceptions/FileNotExistsException.php';
 
 class Configuration
 {
--- LibraryTest.php
+++ LibraryTest.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "LibraryNotExistsException.php";
+include_once "exceptions/LibraryNotExistsException.php";
 
 class LibraryTest
 {
